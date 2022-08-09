404e3ec5864d3845f2ed3551cf16a744d9254136
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-09 15:10:44 +0300

    Add files via upload

---
+++ zarOyunu/zarOyunu/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace zarOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblZar2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblZar1 = new System.Windows.Forms.Label();
+            this.btnZarAt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(466, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Zar:";
+            // 
+            // lblZar2
+            // 
+            this.lblZar2.AutoSize = true;
+            this.lblZar2.Location = new System.Drawing.Point(507, 90);
+            this.lblZar2.Name = "lblZar2";
+            this.lblZar2.Size = new System.Drawing.Size(0, 13);
+            this.lblZar2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1. Zar:";
+            // 
+            // lblZar1
+            // 
+            this.lblZar1.AutoSize = true;
+            this.lblZar1.Location = new System.Drawing.Point(157, 90);
+            this.lblZar1.Name = "lblZar1";
+            this.lblZar1.Size = new System.Drawing.Size(0, 13);
+            this.lblZar1.TabIndex = 2;
+            // 
+            // btnZarAt
+            // 
+            this.btnZarAt.Location = new System.Drawing.Point(267, 213);
+            this.btnZarAt.Name = "btnZarAt";
+            this.btnZarAt.Size = new System.Drawing.Size(75, 23);
+            this.btnZarAt.TabIndex = 3;
+            this.btnZarAt.Text = "Zar At";
+            this.btnZarAt.UseVisualStyleBackColor = true;
+            this.btnZarAt.Click += new System.EventHandler(this.btnZarAt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 395);
+            this.Controls.Add(this.btnZarAt);
+            this.Controls.Add(this.lblZar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblZar2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblZar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblZar1;
+        private System.Windows.Forms.Button btnZarAt;
+    }
+}
+
---
+++ zarOyunu/zarOyunu/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zarOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnZarAt_Click(object sender, EventArgs e)
+        {
+            int number1;
+            int number2;
+            string message= Play(out number1,out number2);
+            MessageBox.Show(message);
+            lblZar1.Text = number1.ToString();
+            lblZar2.Text = number2.ToString();
+
+        }
+
+        private string Play(out int number1, out int number2)
+        {
+            number1 = random.Next(1, 7);
+            number2 = random.Next(1, 7);
+            string message;
+            if (number1 == number2)
+            {
+                message = "Kazandiniz";
+            }
+            else
+            {
+                 message = "Kaybettiniz";
+            }
+
+            return message;
+
+        }
+    }
+}
